2d32da11fc64c2fcc7a464b15c1fde8c8dca8f35
DervisevicAdnan <44082091+DervisevicAdnan@users.noreply.github.com>
2024-11-10 19:10:02 +0100

    Implementiran servis za upravljanje troskovima i konzolni interfejs za koristenje istog

--- StedoMedo/StedoMedo/Program.cs
+++ StedoMedo/StedoMedo/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using StedoMedo.Data;
+using StedoMedo.Models;
+using StedoMedo.Services.UpravljanjeTroskovima;
+using StedoMedo.UI;
+
+Korisnik k = new Korisnik(0, "ado", "Adnan", "Dervisevic", "0600000", "aa", "saagbrwe");
+DbClass db = new DbClass();
+IServisUpravljanjeTroskovima servisUpravljanjeTroskovima = new ServisUpravljanjeTroskovima(db);
+KonzolaUpravljanjeTroskovima upravljanjeTroskovima = new KonzolaUpravljanjeTroskovima(servisUpravljanjeTroskovima,k);
+upravljanjeTroskovima.PokreniInterakcijuKonzole();
