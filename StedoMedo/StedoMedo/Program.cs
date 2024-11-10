5ec8457286b78fdcbe0e7352ee0c78ea2b94927a
Adi Drakovac <129613464+adidrakovac1@users.noreply.github.com>
2024-11-10 14:38:02 +0100

    Urađene funkcionalnosti za Budzet

--- StedoMedo/StedoMedo/Program.cs
+++ StedoMedo/StedoMedo/Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using StedoMedo.Data;
+using StedoMedo.Models;
+using StedoMedo.Services;
+
+Console.WriteLine("BudzetService................");
+Console.WriteLine("Testing...................");
+Console.WriteLine("20%....................");
+Console.WriteLine("46%.......................");
+Console.WriteLine("78%.....................");
+Console.WriteLine("99%....................");
+Console.WriteLine("Mission completed........");
+try
+{
+    var db = new DbClass();
+    var budzetService = new BudzetService(db);
+
+    Korisnik korisnik = new Korisnik(1, "Adi", "Drakovac", "adrakovac2@etf.unsa.ba", "adrakovac", "hashLozinka", "062-009-537");
+    db.AddKorisnik(korisnik);
+    
+    budzetService.DodajBudzet(korisnik, new DateOnly(2024, 11, 1), new DateOnly(2024, 12, 1), 5000);
+    Trosak trosak = new Trosak(1, korisnik, DateTime.Parse("2024-11-10"), 2500, KategorijaTroska.Hrana, "Kupovina hrane za nedelju");
+    Trosak trosak2 = new Trosak(2, korisnik, DateTime.Parse("2024-11-02"), 2000, KategorijaTroska.Prijevoz, "Kupovina karte za prevoz");
+    Trosak trosak3 = new Trosak(3, korisnik, DateTime.Parse("2024-12-01"), 250, KategorijaTroska.Rezije, "Placanje rezija");
+    db.AddTrosak(trosak);
+    db.AddTrosak(trosak2);
+    db.AddTrosak(trosak3);
+
+    double preostaloStanje = budzetService.PreostaloStanjeBudzeta(korisnik, new DateOnly(2024, 12, 1));
+
+    Console.WriteLine($"Preostalo stanje budžeta: {preostaloStanje}");
+}
+catch(Exception e){
+    Console.WriteLine(e.Message);
+}
--- StedoMedo/StedoMedo/obj/Debug/net8.0/StedoMedo.AssemblyInfo.cs
+++ StedoMedo/StedoMedo/obj/Debug/net8.0/StedoMedo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StedoMedo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06f602c02972fd72a90a1454e25e63a864c76510")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f32810e795a07c642de885e71172483e6421d42")]
 [assembly: System.Reflection.AssemblyProductAttribute("StedoMedo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StedoMedo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
