436626c542e32912dde50d8420725109714969f1
azrapamuk <90712839+azrapamuk@users.noreply.github.com.>
2024-11-21 11:39:18 +0100

    ispravke u konzoli

--- StedoMedo/StedoMedo/obj/Debug/net8.0/StedoMedo.AssemblyInfo.cs
+++ StedoMedo/StedoMedo/obj/Debug/net8.0/StedoMedo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StedoMedo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fcb5062353c0aa8b0b61c2d66bd0437a0b130f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67a374a7af72533fe511bfd0901c79833bac43d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("StedoMedo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StedoMedo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
