352cfbe7505a3a8cb4da5f6fa9cd72374553e6c5
aljaljak2 <146694584+aljaljak2@users.noreply.github.com>
2024-11-21 10:38:59 +0100

    Testovi na validnost username, Ime i Prezime

--- StedoMedo/StedoMedoTests/TDDUredjivanjeProfilaTest.cs
+++ StedoMedo/StedoMedoTests/TDDUredjivanjeProfilaTest.cs
@@ -1,6 +1,7 @@
 ﻿using Moq;
 using StedoMedo.Data;
 using StedoMedo.Models;
+using StedoMedo.Services.ServisAutentifikacija;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,26 +10,68 @@ using System.Threading.Tasks;
 
 namespace StedoMedoTests
 {
-    [TestClass]
-    public class TDDUredjivanjeProfilaTest
+    namespace StedoMedoTests
     {
-        [TestMethod]
-        public void proba()
+        [TestClass]
+        public class TDDUredjivanjeProfilaTest
         {
-            // Kreiraj mock
-            var dbClassMock = new Mock<IDbClass>();
-
-            // Mockuj svojstvo Korisnici
-            dbClassMock.Setup(db => db.Korisnici).Returns(new List<Korisnik>
+            private Mock<IDbClass> dbClassMock;
+            private Korisnik testKorisnik;
+            private ServisAutentifikacija servis;
+            [TestInitialize]
+            public void Setup()
             {
-                new Korisnik(1,"","Ivan","Markić","","","")
-            });
 
-            // Testiraj dohvaćanje korisnika
-            var korisnik = dbClassMock.Object.Korisnici.FirstOrDefault(k => k.Id == 1);
+                testKorisnik = new Korisnik(1, "dandadan", "Adnan", "Pršut", "+387123456", "adnan_prsut@hotmail.com", "sifra123");
 
-            Assert.IsNotNull(korisnik);
-            Assert.AreEqual("Ivan", korisnik.Ime);
+                dbClassMock = new Mock<IDbClass>();
+
+                dbClassMock.Setup(db => db.Korisnici).Returns(new List<Korisnik> { testKorisnik });
+                servis = new ServisAutentifikacija(dbClassMock.Object);
+            }
+
+            [TestMethod]
+            public void UredjivanjeKorisnika_ValidniPodaci_Prolazi()
+            {
+                Korisnik noviKorisnik = new Korisnik(1, "adnan", "Adnan", "Pršut", "+387123456", "adnan_prsut@gmail.com", "sifra123");
+                Korisnik rez = servis.UredjivanjeProfila(noviKorisnik);
+                Assert.AreEqual(novi, rez);
+
+            }
+            [TestMethod]
+            [ExpectedException(typeof(ArgumentException))]
+            public void UredjivanjeKorisnika_NeispravanID_DizeException()
+            {
+                Korisnik noviKorisnik = new Korisnik(55, "adnan", "Adnan", "Pršut", "+387123456", "adnan_prsut@gmail.com", "sifra123");
+                Korisnik rez = servis.UredjivanjeProfila(noviKorisnik);
+
+            }
+            [TestMethod]
+            [ExpectedException(typeof(ArgumentException))]
+            public void UredjivanjeKorisnika_NeispravanUsername_DizeException()
+            {
+                Korisnik noviKorisnik = new Korisnik(1, "1adnan", "Adnan", "Pršut", "+387123456", "adnan_prsut@gmail.com", "sifra123");
+                Korisnik rez = servis.UredjivanjeProfila(noviKorisnik);
+            }
+            [TestMethod]
+            [ExpectedException(typeof(ArgumentException))]
+            public void UredjivanjeKorisnika_NeispravanoIme_DizeException()
+            {
+                Korisnik noviKorisnik = new Korisnik(1, "adnan", "1Adnan", "Pršut", "+387123456", "adnan_prsut@gmail.com", "sifra123");
+                Korisnik rez = servis.UredjivanjeProfila(noviKorisnik);
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(ArgumentException))]
+            public void UredjivanjeKorisnika_NeispravanoPrezime_DizeException()
+            {
+                Korisnik noviKorisnik = new Korisnik(55, "adnan", "Adnan", "1Pršut", "+387123456", "adnan_prsut@gmail.com", "sifra123");
+                Korisnik rez = servis.UredjivanjeProfila(noviKorisnik);
+            }
+
+
         }
     }
 }
+    
+
